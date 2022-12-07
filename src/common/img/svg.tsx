772c1a8fa230109ab0d6445d60cd5fd461a00b7a
feruzm <fmiboy@gmail.com>
2022-12-08 00:00:21 +0530

    fix svg file

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -1685,7 +1685,9 @@ export const downCarouselSvg = (
     strokeLinejoin="round"
   >
     <path d="M9 18l6-6-6-6" />
-    
+  </svg>
+);
+
 export const priceDownSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
